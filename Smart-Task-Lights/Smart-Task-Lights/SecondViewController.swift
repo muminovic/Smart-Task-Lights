85ca7825cdee4359b84b06ed76202d37455bfce0
Kimberly Zhu <kimberly.zhu.2000@gmail.com>
2019-03-23 21:50:11 -0700

    create starter files

---
+++ Smart-Task-Lights/Smart-Task-Lights/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Smart-Task-Lights
+//
+//  Created by Kimberly Zhu on 3/23/19.
+//  Copyright © 2019 UCB-Team-Tech. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
